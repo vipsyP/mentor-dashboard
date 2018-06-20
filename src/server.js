03758faafc924ec9a39fe5be643fd1e2cde5a5d9
megha-responsive-news <megha.as@mountblue.io>
2018-06-20 18:25:47 +0530

    stored data inside db

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
 //***************************mongoose connection *************//
-mongoose.connect("mongodb://localhost/iplData");
+mongoose.connect("mongodb://localhost/mentorsDashboard");
 
 app.use(express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -15,11 +15,24 @@ app.use(function(req, res, next) {
 });
 
 //****************************mongoSchema************************//
-// var mentorsDashboardSchema = new mongoose.Schema({}, { strict: false });
-// var tasks = mongoose.model("tasks",mentorsDashboardSchema);
+ var mentorsDashboardSchema = new mongoose.Schema({
+     task: String,
+     member: String
+ });
+ var tasks = mongoose.model("tasks",mentorsDashboardSchema);
 app.post("/addTask" , function(req,res){
-    console.log("task added");
+    // console.log("task added");
     console.log(req.body);
+    var newTask = new tasks({
+        task: req.body.taskName,
+        member: req.body.teamMember
+    });
+    tasks.create(newTask , function(err, tasks){
+        if(err) console.log(err);
+        else{
+            console.log("inserted "+ newTask);
+        }
+    });
 });
 
 
