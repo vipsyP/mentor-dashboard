eb548c6d60d2db639b850fb1d0148d09c1825ffe
vigneshwaran.k <vigneshwaran.k@gitlb.com>
2018-06-29 21:21:45 +0530

    added styles for the showTasks

--- src/components/ShowTasks.js
+++ src/components/ShowTasks.js
@@ -1,55 +1,73 @@
 import React, { Component } from 'react';
-import EditForm  from './EditForm';
-import {Link, Redirect} from 'react-router-dom';
+import EditForm from './EditForm';
+import { Link, Redirect } from 'react-router-dom';
 
 class ShowTasks extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             tasks: [],
-            edit : false,
-            id : ''
+            edit: false,
+            id: '',
+            done: false
             // members : [],
             // date : []
         }
-       console.log(props);
-       
+        console.log(props);
+
     }
-    handleEdit(id){
+    handleEdit(id) {
         this.setState({
-            edit : true,
-            id :id
+            edit: true,
+            done: true,
+            id: id
         })
+        console.log("Edit: " + this.state.done);
     }
-    handleDelete(id){
+    handleDelete(id) {
         console.log(id);
-        fetch('http://localhost:4000/task/deletetask?id='+id)
-        
-        .then(function(response) {
-            return response.json();
+        fetch('http://localhost:4000/task/deletetask?id=' + id)
+
+            .then(function (response) {
+                return response.json();
+            })
+            .then(response => {
+                console.log(response);
+                this.getTasks(response)
+            })
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            })
+        this.setState({
+            done: true
         })
-        .then(response => {
-            console.log(response);
-            this.getTasks(response)
-        }) 
-        .catch(function(err) {
-            console.log('Fetch Error :-S', err);
-        })
+        console.log("Delete: " + this.state.done);
     }
-    componentDidMount(){
+    componentDidMount() {
         fetch('http://localhost:4000/mentor/tasks')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(response => {
-            console.log(response);
-            this.getTasks(response)
-        }) 
-        .catch(function(err) {
-            console.log('Fetch Error :-S', err);
-        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(response => {
+                console.log(response);
+                this.getTasks(response)
+            })
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            })
     }
-    getTasks(res){
+
+
+    shouldComponentUpdate(prevState) {
+        console.log("Prev: " + prevState.done);
+        console.log("Upd: " + this.state.done);
+        return true;
+    }
+
+
+
+
+    getTasks(res) {
         console.log(res);
         // var taskList=[],members=[],date=[];
         var groups = {};
@@ -57,54 +75,53 @@ class ShowTasks extends Component {
             {
                 var groupName = res[i].member;
                 if (!groups[groupName]) {
-                     groups[groupName] = [];
+                    groups[groupName] = [];
                 }
-             groups[groupName].push({task: res[i].task , dueDate: res[i].dueDate, id:res[i]._id});
+                groups[groupName].push({ task: res[i].task, dueDate: res[i].dueDate, id: res[i]._id });
             }
-        }   
-        let myArray=[];
-        for (groupName in groups) {
-            myArray.push({member: groupName, tasks: groups[groupName]});
         }
-    
+        let myArray = [];
+        for (groupName in groups) {
+            myArray.push({ member: groupName, tasks: groups[groupName] });
+        }
+
         console.log(myArray);
         this.setState({
-            tasks : myArray
+            tasks: myArray
         })
-   
+
     }
-  render() {
-    let taskList = this.state.tasks;
-    // let members = this.state.members;
-    // let date = this.state.date;
-    console.log(taskList);
-        return(
+    render() {
+        let taskList = this.state.tasks;
+        console.log(taskList);
+        console.log("Done: " + this.state.done);
+        return (
             <div>{
-                taskList.map((item,i)=>{
+                taskList.map((item, i) => {
                     // console.log("inside map"+item);
                     return (
-                    <div className="assignedTasks">
-                        <div className="member">{item.member}</div>{
-                        item.tasks.map((val,i)=>{
-                           return <div>
-                               <span>{val.task}</span>
-                                <div className="editDelete">
-                                    <input  type="button" onClick={this.handleEdit.bind(this,val.id)} value="Edit" />
-                                    {this.state.edit && this.state.id===val.id ? <EditForm  id={val.id} /> :""}
-                                    <input onClick={this.handleDelete.bind(this,val.id)} type="button" value="Delete"/>
-                                 </div>
-                                <span className="due-date">Due date: {val.dueDate}</span>
-                                </div>
-                        })
-                        }
-                    </div>
+                        <div className="assignedTasks">
+                            <div className="member">{item.member}</div>{
+                                item.tasks.map((val, i) => {
+                                    return <div>
+                                        <span>{val.task}<span className="due-date">Due date: {val.dueDate}</span></span>
+                                        <div className="editDelete">
+                                            <input type="button" onClick={this.handleEdit.bind(this, val.id)} value="Edit" />
+                                            {this.state.edit && this.state.id === val.id ? <EditForm id={val.id} /> : ""}
+                                            <input onClick={this.handleDelete.bind(this, val.id)} type="button" value="Delete" />
+                                        </div>
+                                        <hr />
+                                    </div>
+                                })
+                            }
+                        </div>
                     )
                 })
 
             }
-            </div>            
+            </div>
         )
-  }
+    }
 }
 
 export default ShowTasks;
